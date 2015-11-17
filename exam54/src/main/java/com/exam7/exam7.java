a1e8817db78ea65a390fd7953b16006a12be5a14
lijinchao <lijinchao2007@163.com>
2015-11-17 17:44:08 +0800

    fix

---
+++ exam54/src/main/java/com/exam7/exam7.java
@@ -0,0 +1,10 @@
+package com.exam7;
+
+// import com.exam45;
+public class exam7 {
+   //  public static void main(String args[]) {
+   //     System.out.println("Hello World!");
+   //
+   //  //    Toy().method();
+   // }
+}
